f4c7fbcb72616259a601a80ae042e9dba488af21
aadeee1 <atif8184@gmail.com>
2024-03-25 16:08:33 +0500

    Push my cli-number-guessing game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+let guessingNumber =  await inquirer.prompt([
+    {
+      message: "Enter your Number Between 1-10:",
+      type: "number",
+      name: "YourGuess",
+    },
+  ]);
+    // conditional test
+    if(guessingNumber.YourGuess === randomNumber){
+      console.log("congratulations! You Guess the Right...........");
+    }
+    else{
+      console.log("Oops! you guess Wrong Number........");
+    }
